066eeffb2e9e144f261bb5813b4c1b2ba0193f24
ValRod-Sal <valeria.rs.rodriguez@gmail.com>
2025-10-15 00:10:44 -0400

    JORGEEEEE

--- pruba.cpp
+++ pruba.cpp
@@ -4,5 +4,6 @@ int main() {
     cout << "Hola, Mundo!" << endl;
     cout << "Probando GitHub" << endl;
     cout << "primeros pasos banca movil apk 2025 free to play" << endl;
+    cout<<"Se mamo el Jorge"<<endl;
     return 0;
 }
